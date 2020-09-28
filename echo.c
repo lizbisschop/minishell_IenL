280fc3bb3e6efdd8dd2d46d1ff241e4ab9ba8bf1
Iris <iboeters@LAPTOP-MGJ0DHHN.localdomain>
2020-09-28 16:44:27 +0200

    echo changed

--- echo.c
+++ echo.c
@@ -18,9 +18,9 @@ void		echo(t_command command)
 		while (i < command.tok_amount)
 		{
 			// printf("%c|%i|%i\n", command.tokens[i][0], i, command.tok_amount);
-			command.tokens[i] = unquote(&command.tokens[i]);
 			if (is_delimiter(command.tokens[i][0]))
 				break ;
+			command.tokens[i] = unquote(&command.tokens[i]);
 			ft_putstr_fd(command.tokens[i], 1);
 			ft_putchar_fd(' ', 1);
 			i++;
