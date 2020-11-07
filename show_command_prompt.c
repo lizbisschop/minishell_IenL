f3b5a0db985df62483bdff8691ca728e01f7fb8e
Liz Bisschop <lbisscho@f1r1s19.codam.nl>
2020-11-07 18:03:54 +0100

    done

--- show_command_prompt.c
+++ show_command_prompt.c
@@ -15,10 +15,10 @@ void	show_command_prompt(void)
 		ft_putstr_fd("Error getting path\n", 2);
 		exit(1);
 	}
-	ft_putstr_fd("\e[0;33m~", 1); //eruit voor eval
-	ft_putstr_fd(buf, 1);
-	ft_putstr_fd("\e[0m", 1); //eruit voor eval
-	write(1, "$ ", 2);
+	ft_putstr_fd("\e[0;33m~", 2); //eruit voor eval
+	ft_putstr_fd(buf, 2);
+	ft_putstr_fd("\e[0m", 2); //eruit voor eval
+	write(2, "$ ", 2);
 	if (buf[0] != '\0')
 		free(buf);
 }
