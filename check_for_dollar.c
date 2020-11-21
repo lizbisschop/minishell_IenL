1e54343b85381ecf209d759099977a6236123586
Iris <iboeters@LAPTOP-MGJ0DHHN.localdomain>
2020-11-21 16:18:37 +0100

    updated

--- check_for_dollar.c
+++ check_for_dollar.c
@@ -6,7 +6,7 @@
 /*   By: iboeters <iboeters@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/11/09 18:16:11 by iboeters      #+#    #+#                 */
-/*   Updated: 2020/11/21 10:30:01 by iboeters      ########   odam.nl         */
+/*   Updated: 2020/11/21 16:18:16 by iboeters      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
